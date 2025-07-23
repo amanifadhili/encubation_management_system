546ab5be805f8747e559948013394ba5a344b47e
amanifadhili <a.fadhiliprojects@gmail.com>
2025-07-23 16:13:02 +0200

    node change

---
+++ scripts/copy-redirects.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+
+const src = path.join(__dirname, '../public/_redirects');
+const dest = path.join(__dirname, '../build/client/_redirects');
+
+fs.copyFileSync(src, dest);
+console.log(`Copied _redirects from ${src} to ${dest}`); 
